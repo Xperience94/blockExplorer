540ed1e34dc97fa33b9aaa7746cfe8a71d7760ef
hugolopes <hugoalexandrelopes2@gmail.com>
2023-12-23 11:52:45 +0100

    week3 step5

--- src/App.js
+++ src/App.js
@@ -1,36 +1,115 @@
 import { Alchemy, Network } from 'alchemy-sdk';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './App.css';
 
-// Refer to the README doc for more information about using API
-// keys in client-side code. You should never do this in production
-// level code.
 const settings = {
   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
 };
 
-
-// In this week's lessons we used ethers.js. Here we are using the
-// Alchemy SDK is an umbrella library with several different packages.
-//
-// You can read more about the packages here:
-//   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
 const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [blockInfo, setBlockInfo] = useState();
 
   useEffect(() => {
-    async function getBlockNumber() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
+    async function fetchData() {
+      try {
+        // Get the current block number
+        const currentBlockNumber = await alchemy.core.getBlockNumber();
+        setBlockNumber(currentBlockNumber);
+
+        // Get information about a specific block (e.g., the latest block)
+        const block = await alchemy.core.getBlockWithTransactions(currentBlockNumber);
+
+        // Set block information to state
+        setBlockInfo(block);
+
+        // Log block information
+        console.log("Block Information:", block);
+
+        // Loop through transactions in the block
+        if (block && block.transactions) {
+          for (const transaction of block.transactions) {
+            // Access transaction properties
+            console.log("affichage de transaction",transaction);
+            const {
+              hash,
+              from,
+              to,
+              value,
+              gasPrice,
+              // Add more properties as needed
+            } = transaction;
+
+            // Perform actions with transaction properties
+            console.log("Transaction Hash:", hash);
+            console.log("From:", from);
+            console.log("To:", to);
+            console.log("Value:", value);
+            console.log("Gas Price:", gasPrice);
+            console.log("------------------------");
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     }
 
-    getBlockNumber();
-  });
+    fetchData();
+  }, []); // Empty dependency array to ensure the effect runs only once on mount
+return (
+  <div className="App">
+  <div>Block Number: {blockNumber}</div>
+  {blockInfo && (
+    <div>
+      <h2>Block Information:</h2>
+      <div>
+        <strong>Block Hash:</strong> {blockInfo.hash}
+      </div>
+      <div>
+        <strong>Timestamp:</strong> {blockInfo.timestamp}
+      </div>
+      <div>
+        <strong>Miner:</strong> {blockInfo.miner}
+      </div>
+      <div>
+        <strong>Gas Limit:</strong> {blockInfo.gasLimit.toString()}
+      </div>
+      <div>
+        <strong>Gas Used:</strong> {blockInfo.gasUsed.toString()}
+      </div>
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+      <h2>Transactions:</h2>
+      <ul>
+        {blockInfo.transactions.map((transaction, index) => (
+          <li key={index}>
+            <div>
+              <strong>Transaction Hash:</strong> {transaction.hash}
+            </div>
+            <div>
+              <strong>From:</strong> {transaction.from}
+            </div>
+            <div>
+              <strong>To:</strong> {transaction.to}
+            </div>
+            <div>
+              <strong>Value:</strong> {transaction.value.toString()} {/* Convert to string */}
+            </div>
+            
+            <div>
+              <strong>Gas Price:</strong> {transaction.gasPrice.toString()} {/* Convert to string */}
+            </div>
+            <hr />
+          </li>
+        ))}
+      </ul>
+    </div>
+  )}
+</div>
+);
 }
 
 export default App;
